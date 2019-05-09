c3689b5b9e6ef30eb36e08b6c01ef7db7d69479c
Ivo van Dongen <info@ivovandongen.nl>
2019-05-09 16:40:00 +0300

    gtest - proper pthreads build config

--- deps/googletest.cmake
+++ deps/googletest.cmake
@@ -2,6 +2,12 @@ include_guard_x()
 
 set(GTEST_DIR ${CMAKE_SOURCE_DIR}/deps/googletest/googletest)
 
+set(THREADS_PREFER_PTHREAD_FLAG ON)
+find_package(Threads)
+if (CMAKE_USE_PTHREADS_INIT)
+    set(GTEST_HAS_PTHREAD ON)
+endif()
+
 add_library(gtest STATIC
         ${GTEST_DIR}/src/gtest.cc
         ${GTEST_DIR}/src/gtest-death-test.cc
@@ -20,3 +26,5 @@ target_include_directories(gtest
 target_include_directories(gtest
         SYSTEM INTERFACE ${GTEST_DIR}/include
         )
+
+target_link_libraries(gtest PUBLIC Threads::Threads)
