5551ffde55ddfe1c7c5ac3ca7f759e7e37a74547
Ivo van Dongen <info@ivovandongen.nl>
2019-06-19 09:18:00 +0300

    [imgui] add dependency

---
+++ deps/imgui.cmake
@@ -0,0 +1,17 @@
+include_guard_x()
+
+set(IMGUI_DIR ${CMAKE_SOURCE_DIR}/deps/imgui)
+
+add_library(imgui STATIC
+        ${IMGUI_DIR}/imgui.cpp
+        ${IMGUI_DIR}/imgui_draw.cpp
+        ${IMGUI_DIR}/imgui_widgets.cpp
+        )
+
+target_include_directories(imgui
+        PRIVATE ${IMGUI_DIR}
+        )
+
+target_include_directories(imgui
+        SYSTEM INTERFACE ${IMGUI_DIR}
+        )
