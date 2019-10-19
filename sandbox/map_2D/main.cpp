26d29c24d0d517f24ae4342a0b2969afc2475a89
Ivo van Dongen <info@ivovandongen.nl>
2019-10-19 12:11:47 +0300

    [sandbox] add scroll handler and imgui to map2d

--- sandbox/map_2D/main.cpp
+++ sandbox/map_2D/main.cpp
@@ -1,9 +1,13 @@
+#include <glbr/imgui/imgui_layer.hpp>
 #include <glbr/input/events/key_event.hpp>
+#include <glbr/input/events/mouse_scroll_event.hpp>
 #include <glbr/logging/logging.hpp>
 #include <glbr/renderer/glfw/graphics_window_glfw.hpp>
 #include <glbr/renderer/opengl3/device_opengl3.hpp>
 
 #include "map_2d.hpp"
+
+#include <imgui.h>
 
 int main() {
     int width = 512, height = 512;
@@ -44,7 +48,7 @@ int main() {
     auto eventHandlerReg = window.registerHandler([&](Event& event) {
         EventDispatcher d(event);
 
-        return d.dispatch<KeyEvent>([&](KeyEvent& e) {
+        bool handled = d.dispatch<KeyEvent>([&](KeyEvent& e) {
             if (e.state() == KeyState::PRESS) {
                 switch (e.keyCode()) {
                     case KeyCode::KEY_Q:
@@ -59,24 +63,50 @@ int main() {
             }
             return false;
         });
+
+        handled |= d.dispatch<MouseScrollEvent>([&](MouseScrollEvent& e) {
+            logging::info("Scroll event: {}x{}", e.offsetX(), e.offsetY());
+            map.zoom(map.zoom() - e.offsetY());
+            return true;
+        });
+
+        return handled;
     });
+
+    // IMGui layer
+    imgui::ImGuiLayer imguiLayer{window};
+    imguiLayer.addRenderable([&]() {
+        static std::array<char, 10> buf;
+        ImGui::SetNextWindowPos({0, 0});
+        ImGui::SetNextWindowSize({float(width), 100});
+        ImGui::Begin("Map state", nullptr, ImGuiWindowFlags_None);
+        auto bounds = map.bounds();
+        ImGui::Text("Bounds: [%.3f,%.3f],[%.3f,%.3f]",
+                    bounds.ne().lng(),
+                    bounds.ne().lat(),
+                    bounds.sw().lng(),
+                    bounds.sw().lat());
+        float zoom = map.zoom();
+        if (ImGui::SliderFloat("", &zoom, map.minZoom(), 22, "Zoom: %.3f")) {
+            map.zoom(zoom);
+        }
+        ImGui::Text("Min Zoom: %.3f", map.minZoom());
+        ImGui::End();
+    });
+
+    imguiLayer.init(window.context());
 
     // Render function
     auto renderFn = [&](renderer::Context& context) {
         // Clear the scene
         context.clear(clearState);
 
-        auto bounds = map.bounds();
-        logging::debug("Bounds: ne:{},{} sw:{},{}, zoom: {}",
-                       bounds.ne().x(),
-                       bounds.ne().y(),
-                       bounds.sw().x(),
-                       bounds.sw().y(),
-                       map.zoom());
+        // Render gui
+        imguiLayer.render(context, sceneState);
     };
 
     // Update function
-    auto updateFn = [&](auto interval) {};
+    auto updateFn = [&](auto interval) { imguiLayer.update(interval, sceneState); };
 
     // Runloop
     window.run(renderFn, updateFn, 60);
